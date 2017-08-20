66c8533144f5098ad6f2ad034c86d336c8a7c57c
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-20 22:07:23 +0800

    change index.js to CRLF and remove return out of function

--- index.js
+++ index.js
@@ -8,38 +8,37 @@ var db = false;
 
 if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASS || !process.env.DB_CONFIG) {
   module.exports = db;
-  return;
+} else {
+  db = {};
+  var sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    JSON.parse(process.env.DB_CONFIG)
+  );
+
+  const MODELS_PATH = path.join(__dirname, 'models');
+  fs
+    .readdirSync(MODELS_PATH)
+    .filter((file) => {
+      // skip all filenames that start with a '.'
+      return (file.indexOf(".") !== 0);
+    })
+    .forEach((file) => {
+      var model = sequelize.import(path.join(MODELS_PATH, file));
+      db[model.name] = model;
+    });
+
+  Object
+    .keys(db)
+    .forEach((modelName) => {
+      if ("associate" in db[modelName]) {
+        db[modelName].associate(db);
+      }
+    });
+
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+
+  module.exports = db;
 }
-
-db = {};
-var sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  JSON.parse(process.env.DB_CONFIG)
-);
-
-const MODELS_PATH = path.join(__dirname, 'models');
-fs
-  .readdirSync(MODELS_PATH)
-  .filter((file) => {
-    // skip all filenames that start with a '.'
-    return (file.indexOf(".") !== 0);
-  })
-  .forEach((file) => {
-    var model = sequelize.import(path.join(MODELS_PATH, file));
-    db[model.name] = model;
-  });
-
-Object
-  .keys(db)
-  .forEach((modelName) => {
-    if ("associate" in db[modelName]) {
-      db[modelName].associate(db);
-    }
-  });
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
