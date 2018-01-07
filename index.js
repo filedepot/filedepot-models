0ab86b85cae3e071e5d7a7637e5a3eaaeeb4e86a
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-07 16:27:43 +0000

    update models index

--- index.js
+++ index.js
@@ -4,41 +4,37 @@ const Sequelize = require("sequelize");
 
 var db = false;
 
-if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASS || !process.env.DB_CONFIG) {
-  module.exports = db;
-} else {
-  db = {};
-  var sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    JSON.parse(process.env.DB_CONFIG)
-  );
+db = {};
+var sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  JSON.parse(process.env.DB_CONFIG)
+);
 
-  const MODELS_PATH = path.join(__dirname, 'models');
+const MODELS_PATH = path.join(__dirname, 'models');
 
-  // eslint-disable-next-line no-sync
-  fs
-    .readdirSync(MODELS_PATH)
-    .filter((file) => {
-      // skip all filenames that start with a '.'
-      return file.indexOf(".") !== 0;
-    })
-    .forEach((file) => {
-      var model = sequelize.import(path.join(MODELS_PATH, file));
-      db[model.name] = model;
-    });
+// eslint-disable-next-line no-sync
+fs
+  .readdirSync(MODELS_PATH)
+  .filter((file) => {
+    // skip all filenames that start with a '.'
+    return file.indexOf(".") !== 0;
+  })
+  .forEach((file) => {
+    var model = sequelize.import(path.join(MODELS_PATH, file));
+    db[model.name] = model;
+  });
 
-  Object
-    .keys(db)
-    .forEach((modelName) => {
-      if ("associate" in db[modelName]) {
-        db[modelName].associate(db);
-      }
-    });
+Object
+  .keys(db)
+  .forEach((modelName) => {
+    if ("associate" in db[modelName]) {
+      db[modelName].associate(db);
+    }
+  });
 
-  db.sequelize = sequelize;
-  db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-  module.exports = db;
-}
+module.exports = db;
